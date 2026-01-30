7c60844b89957a0b710becadd4f68254144cdd68
drishyaanilcode <drishyaanil9396@gmail.com>
2026-01-30 21:22:53 +0400

    docs: add primitive types demonstration

---
+++ src/fundamentals/Variables.java
@@ -0,0 +1,15 @@
+package fundamentals;
+
+public class Variables {
+    public static void primitiveTypes() {
+        byte age = 25;
+        int salary = 85000;
+        double pi = 3.14159;
+        boolean isEmployed = true;
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        primitiveTypes();
+    }
+}
